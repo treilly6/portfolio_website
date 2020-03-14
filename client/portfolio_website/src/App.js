83161559a66ddb397a80c28a2698cae1cdb18f7f
treilly6 <treilly6@binghamton.edu>
2020-03-14 12:21:56 -0400

    basic starting components

--- client/portfolio_website/src/App.js
+++ client/portfolio_website/src/App.js
@@ -1,12 +1,66 @@
 import React from 'react';
 import './App.css';
+import gif1 from './media/sample.gif';
+import ProjectCards from './components/ProjectCards';
 
 function App() {
   return (
     <div className="App">
-      <div>HERE MY REACT APP</div>
+      <div style={gifContainer} >
+          <img src={gif1} style={gif} />
+          {/* <div style={infoBox}>
+              <div style={{fontSize : "10vw"}}>Thomas Reilly</div>
+              <div style={{fontSize : "3vw"}}>Software Engineer</div>
+              <div>
+
+              </div>
+              <div style={viewProjBtn}>View Projects</div>
+          </div> */}
+      </div>
+      <div className="section">
+          <div className="head-text">
+              <h2 className="gradient-color">Project Portfolio</h2>
+              <ProjectCards />
+          </div>
+      </div>
+
     </div>
   );
+}
+
+const gifContainer = {
+    position : "relative",
+}
+
+const gif = {
+    width : "100%",
+    position : "relative",
+    top : "-10px",
+    transform: "skewY(-2deg)",
+}
+
+const infoBox = {
+    position: "absolute",
+    top : "50%",
+    left : "50%",
+    transform: "translate(-50%, -50%)",
+    width : "100%",
+    /* font-weight: bold; */
+    /* color : #ff8c1a; */
+    color : "#fff",
+    textAlign: "center",
+
+}
+
+const viewProjBtn = {
+    border : "1px solid #fff",
+    padding : "1vw",
+    borderRadius : "5px",
+    maxWidth : "20vw",
+    margin : "0 auto",
+    cursor : "pointer",
+    fontSize : "2vw",
+    marginTop : "8px",
 }
 
 export default App;
---
+++ client/portfolio_website/src/components/ProjectCards.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+// put an array of the projects up here that will be looped through to render a card for each project
+// const projects = [
+//     {
+//         name : 'project_name',
+//         description : 'project descriptions',
+//         skills : ['skills', 'used', 'in', 'project'],
+//     },
+//     {
+//         name : 'project_name',
+//         description : 'project descriptions',
+//         skills : ['skills', 'used', 'in', 'project'],
+//     },
+// ]
+
+export default function ProjectCards() {
+
+    return(
+        <div>
+            PROJECT CARD COMPONENTS
+        </div>
+    )
+}
